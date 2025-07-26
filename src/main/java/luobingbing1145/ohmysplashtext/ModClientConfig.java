f8a90412ed6f3296bac66d94efa21cc1f39d6332
MCDm14514 <a18328415133@163.com>
2025-07-26 19:03:17 +0800

    提升函数预览性能

--- src/main/java/luobingbing1145/ohmysplashtext/ModClientConfig.java
+++ src/main/java/luobingbing1145/ohmysplashtext/ModClientConfig.java
@@ -393,7 +393,7 @@ public class ModClientConfig {
                                                         .description(OptionDescription.of(Text.translatable("config.ohmysplashtext.option.gragh.desc")))
                                                         .action((yaclScreen, buttonOption) -> {
                                                             DoubleUnaryOperator parser = MathExpressionParser.parse(config.getFunctionOfSplashingAnim());
-                                                            MinecraftClient.getInstance().setScreen(new GraphScreen(makeScreen(parent), parser, 0.02, 0.02, 0.05, 20));
+                                                            MinecraftClient.getInstance().setScreen(new GraphScreen(makeScreen(parent), parser, 0.01, 0.02, 0.1, 20));
                                                         })
                                                         .available(config.isSplashTextEnable() && config.isAdvancedModeEnable() && config.isSplashingAnimEnable())
                                                         .build()
@@ -430,7 +430,7 @@ public class ModClientConfig {
                                                         .description(OptionDescription.of(Text.translatable("config.ohmysplashtext.option.gragh.desc")))
                                                         .action((yaclScreen, buttonOption) -> {
                                                             DoubleUnaryOperator parser = MathExpressionParser.parse(config.getFunctionOfRotationAnim());
-                                                            MinecraftClient.getInstance().setScreen(new GraphScreen(makeScreen(parent), parser, 0.02, 0.5, 0.05, 20));
+                                                            MinecraftClient.getInstance().setScreen(new GraphScreen(makeScreen(parent), parser, 0.01, 0.5, 0.1, 20));
                                                         })
                                                         .available(config.isSplashTextEnable() && config.isAdvancedModeEnable() && config.isRotationAnimEnable())
                                                         .build()
